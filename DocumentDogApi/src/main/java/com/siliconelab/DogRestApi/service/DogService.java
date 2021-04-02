3cecfefd0a939d4cd76d40740fea2832c6ae0f9c
muhammet-sahin-ITU <muhammetsahin@itu.edu.tr>
2021-04-02 20:43:30 +0300

    Initial commit

---
+++ DocumentDogApi/src/main/java/com/siliconelab/DogRestApi/service/DogService.java
@@ -0,0 +1,12 @@
+package com.siliconelab.DogRestApi.service;
+
+import com.siliconelab.DogRestApi.entity.Dog;
+
+import java.util.List;
+
+public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
